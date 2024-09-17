542b1c13930c1cb1f9ee67123e3b5c3e09a832c5
Loïc Marchand <loic.marchand@epfedu.fr>
2024-09-17 16:57:08 +0200

    Création d'un nouveau projet versionné

---
+++ TP0_Exo1/TP0_Exo1/src/tp0_exo1/TP0_Exo1.java
@@ -0,0 +1,36 @@
+/*
+* TP0 Ex 1
+* Marchand Loïc 
+* TDC
+* Septembre 2024
+ */
+package tp0_exo1;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author loicm
+ */
+public class TP0_Exo1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+         System.out.println("Bonjour");
+         System.out.println("Au revoir");
+         
+         
+         String prenom;
+ Scanner sc;
+ sc = new Scanner(System.in);
+ System.out.println("Bonjour, quel est votre prenom ?");
+ prenom = sc.nextLine();
+         
+         
+        // TODO code application logic here
+    }
+    
+}
